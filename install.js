dd537d0bf816353fda59ebcbe0f7ee0f7c857046
zongze.li <zongze.li@q7link.com>
2019-05-15 13:37:14 +0800

    test: test install pwd

--- install.js
+++ install.js
@@ -2,6 +2,15 @@ const { spawnAsync } = require('./util');
 const fs = require('fs');
 const pwd = process.env.PWD;
 
+fs.readdir('./',(err,files)=>{
+
+	if(err) throw err;
+
+	console.log('readdir ---- ', files);
+
+});
+
+
 spawnAsync('sh', [`./install.sh`, pwd], { stdio: 'inherit' })
 	.then(() => {
 		process.exit(0);
